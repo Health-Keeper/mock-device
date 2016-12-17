6080fb5abfa9da97da423b0351bb5376a09010bc
Sebastian Potasiak <sebastian.potasiak@gmail.com>
2016-12-17 09:23:32 +0100

    Added limits class

---
+++ limits.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class Limits(object):
+    BLOOD_PRESSURE_SYSTOLIC = None, None
+    BLOOD_PRESSURE_DIASTOLIC = None, None
+    PULSE = None, None
+    SATURATION = None, None
+    ELECTRODERMAL_RESPONSE = None, None
+    BODY_TEMPERATURE = None, None
+    BLOOD_BLUCOSE_CONTENT = None, None
+    BLOOD_ALCOHOL_CONTENT = 0, None
+    CHOLESTEROL_LDL = None, None
+    CHOLESTEROL_HDL = None, None
+    GPS_LATITUDE = None, None
+    GPS_LONGITUDE = None, None
